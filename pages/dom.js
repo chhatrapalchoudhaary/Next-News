8e184783f1a7be664e9986452daf2aec0881a659
chhatrapalchoudhaary <chhatrapalchoudhary101@gmail.com>
2021-12-08 09:45:54 +0530

    Next News

---
+++ pages/dom.js
@@ -0,0 +1,13 @@
+const DOM = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export const getServerSideProps = async ()=> {
+    const apiResponse = await fetch()
+}
+
+export default DOM
